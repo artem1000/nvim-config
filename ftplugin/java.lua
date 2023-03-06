c657f7f483e15f5c13448d5658a43bd04072918f
Artem Grinchenko <agrinchenko@datex.ca>
2023-03-06 11:01:07 -0500

    Initial commit of nvim config with Java JDTLS working off OpenJDK 19.0.2

---
+++ ftplugin/java.lua
@@ -0,0 +1,69 @@
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local worksace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), "p:h:t")
+
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local config = {
+	-- The command that starts the language server
+	-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+	cmd = {
+		"/usr/local/opt/openjdk/bin/java",
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.level=ALL",
+		--"-noverify",
+		"-Xmx1G",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
+		"-jar",
+		"/opt/java/JDK/jdt-language-server-1.9.0-202203031534/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
+		"-configuration",
+		"/opt/java/JDK/jdt-language-server-1.9.0-202203031534/config_mac/",
+		"-data",
+		vim.fn.expand("~/.cache/jdtls-workspace") .. worksace_dir,
+	},
+
+	root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+	capabilities = capabilities,
+
+	-- Here you can configure eclipse.jdt.ls specific settings
+	-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+	-- for a list of options
+	settings = {
+		java = {},
+	},
+
+	init_options = {
+		bundles = {},
+	},
+}
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
+require("jdtls").start_or_attach(config)
+
+-- Mappings.
+local opts = { noremap = true, silent = true }
+
+-- See `:help vim.lsp.*` for documentation on any of the below functions
+vim.api.nvim_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+vim.api.nvim_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+vim.api.nvim_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+vimapi.nvim_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+--vim.api.nvim_set_keymap("n", "<space>ldd", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+vim.api.nvim_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+vim.api.nvim_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<space>lg", "<cmd>lua vim.lsp.buf.formatting_sync(nil, 1000)<CR>", opts)
+vim.api.nvim_set_keymap("n", "<space>lA", "require('jdtls').code_action<CR>", { silent = true })
