05ecdb476aa6fad047581107c421f05d6a555c41
Artem Grinchenko <artem1000@gmail.com>
2024-12-26 18:36:55 -0500

    Add Custom File Path title

--- lua/artem/core/workflows.lua
+++ lua/artem/core/workflows.lua
@@ -1,3 +1,6 @@
+vim.cmd("set titlestring=%{hostname()}\\ \\ %F\\ \\ %{strftime('%Y-%m-%d\\ %H:%M',getftime(expand('%')))}")
+vim.cmd("set title")
+
 --------------
 -- obsidian --
 --------------
@@ -31,16 +34,8 @@ vim.keymap.set("n", "<leader>on", ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/
 vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/\\(-\\|_\\)/ /g<cr>")
 --
 -- search for files in full vault
-vim.keymap.set(
-	"n",
-	"<leader>os",
-	':Telescope find_files search_dirs={"/Users/agrinchenko/Documents/Notes/Notes/notes"}<cr>'
-)
-vim.keymap.set(
-	"n",
-	"<leader>oz",
-	':Telescope live_grep search_dirs={"/Users/agrinchenko/Documents/Notes/Notes/notes"}<cr>'
-)
+vim.keymap.set("n", "<leader>os", ':Telescope find_files search_dirs={"~/Documents/Notes/notes"}<cr>')
+vim.keymap.set("n", "<leader>oz", ':Telescope live_grep search_dirs={"~/Documents/Notes/Notes/notes"}<cr>')
 --
 -- search for files in notes (ignore zettelkasten)
 -- vim.keymap.set("n", "<leader>ois", ":Telescope find_files search_dirs={\"/Users/alex/library/Mobile\\ Documents/iCloud~md~obsidian/Documents/ZazenCodes/notes\"}<cr>")
@@ -48,6 +43,6 @@ vim.keymap.set(
 --
 -- for review workflow
 -- move file in current buffer to zettelkasten folder
-vim.keymap.set("n", "<leader>ok", ":!mv '%:p' /Users/agrinchenko/Documents/Notes/Notes/zettelkasten<cr>:bd<cr>")
+vim.keymap.set("n", "<leader>ok", ":!mv '%:p' ~/Documents/Notes/zettelkasten<cr>:bd<cr>")
 -- delete file in current buffer
 vim.keymap.set("n", "<leader>odd", ":!rm '%:p'<cr>:bd<cr>")
--- lua/artem/plugins/nvim-tree.lua
+++ lua/artem/plugins/nvim-tree.lua
@@ -7,6 +7,7 @@ return {
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
+		vim.g.nvim_tree_respect_buf_cwd = 1
 
 		nvimtree.setup({
 			view = {
@@ -45,17 +46,15 @@ return {
 			},
 		})
 
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
-
-		-- keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-		keymap.set(
-			"n",
-			"<leader>ee",
-			"<cmd>NvimTreeFindFileToggle<CR>",
-			{ desc = "Toggle file explorer on current file" }
-		) -- toggle file explorer on current file
-		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+		-- below block ensures that the tree navigates to the currently opened file
+		--
+		update_focused_file =
+			{
+				enable = true,
+				update_cwd = true,
+			}, 
+vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
+		vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+		vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
 	end,
 }
