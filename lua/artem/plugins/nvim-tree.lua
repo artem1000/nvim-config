0952598b191e9677ae0cfa17fb5a18a696a9783c
Artem Grinchenko <artem1000@gmail.com>
2025-03-02 22:47:20 -0500

    Suppress Nvim-tree from showinng up on start and make it floating

--- lua/artem/plugins/nvim-tree.lua
+++ lua/artem/plugins/nvim-tree.lua
@@ -9,11 +9,41 @@ return {
 		vim.g.loaded_netrwPlugin = 1
 		vim.g.nvim_tree_respect_buf_cwd = 1
 
+		local gwidth = vim.api.nvim_list_uis()[1].width
+		local gheight = vim.api.nvim_list_uis()[1].height
+		local width = 80
+		local height = 40
+
 		nvimtree.setup({
-			view = {
-				width = 35,
-				relativenumber = true,
+
+			-- Below block ensures the nvim does not open on starting nvim
+			hijack_unnamed_buffer_when_opening = true,
+			disable_netrw = true, -- disable :Explore
+			hijack_directories = {
+				enable = true,
+				auto_open = false,
 			},
+
+			-- old view config - open on the left side vertically
+			-- view = {
+			-- 	width = 35,
+			-- 	relativenumber = false,
+			-- },
+
+			-- new old view config - floating in the center
+			view = {
+				float = {
+					enable = true,
+					open_win_config = {
+						relative = "editor",
+						width = width,
+						height = height,
+						row = (gheight - height) * 0.4,
+						col = (gwidth - width) * 0.2,
+					},
+				},
+			},
+
 			-- change folder arrow icons
 			renderer = {
 				indent_markers = {
@@ -52,8 +82,7 @@ return {
 			{
 				enable = true,
 				update_cwd = true,
-			}, 
-vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
+			}, vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
 		vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
 		vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
 	end,
